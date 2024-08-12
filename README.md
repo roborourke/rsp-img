fd99424a5a0e32aeb3699cee9b6059e0fb2b89c8
Robert O'Rourke <robert@o-rourke.org>
2024-08-12 19:34:03 +0100

    Updates the sizes attribute, props to @goldenapples

--- README.md
+++ README.md
@@ -1,10 +1,6 @@
 # `rsp-img`
 
-JS that extends the img tag to use `srcset` according to the image's actual rendered size, rather than the viewport.
-
-## tl;dr
-
-Load this JS on a web page, done.
+JS that updates image's `sizes` attribute according to its currently displayed size.
 
 ## Demo
 
@@ -12,15 +8,13 @@ https://gigantic-three-sparrow.glitch.me/
 
 ## What is this?
 
-Responsive images on the web are difficult, cumbersome, and widely misunderstood. What I think most of us would like is an image tag that uses the `srcset` attribute to determine the most appropriate available src based on the size the image is currently being displayed at.
+Responsive images on the web are difficult to get right. What I think most of us would like is an image tag that uses the `srcset` attribute to determine the most appropriate available src based on the size the image is currently being displayed at, rather than in relation to the viewport.
 
-This is unfortunately not really how it works today. Maybe it can improve in future if the `sizes` attributes can support container queries.
-
-This will degrade gracefully in browsers that do not support web components as well, falling back to standard `src`, `srcset` and `sizes` behaviour.
+This library makes the `sizes` attribute dynamic and tied to an image's current display size, so the browser can make a more informed decision about which image to download from the `srcset`.
 
 ## Features
 
-- Less than 1kb
+- Less than 500B
 - Progressive enhancement for standard `img` tags
 - Accounts for device pixel ratio
 - Uses `ResizeObserver` and `requestAnimationFrame` for optimal performance
@@ -32,10 +26,3 @@ Include the `rsp-img.js` file in your bundle or directly in your web page e.g.
 ```html
 <script src="rsp-img.min.js" async></script>
 ```
-
-### Further Optimisation
-
-If you're quietly confident most visitors will have JS enabled, then the following options may be of interest:
-
-- Set the `src` of your images to the smallest available option, or a placeholder as only the `srcset` options are used as candidates for display. If there's any delay loading the JS then only the smallest image will start to be downloaded
-- Set the `srcset` attribute as `data-srcset` instead. This will prevent the browser from automatically inferring and trying to download a larger size before the web component kicks in.
--- rsp-img.js
+++ rsp-img.js
@@ -1,61 +1,34 @@
 const ri = node => {
-  if (!node.srcset && !node.dataset.srcset) {
-      return;
+    // Update the sizes attribute on resize.
+    node.ri = new ResizeObserver((entries) => {
+        requestAnimationFrame(() => {
+            node.sizes = `${entries[0].contentRect.width}px`;
+        });
+    });
+
+    node.ri.observe(node);
   }
 
-  const srcset = node.srcset || node.dataset.srcset;
-  node.srcset = '';
-
-  // Make a list of srcs from largest to smallest.
-  const srcs = srcset.split(',').map(src => {
-      const [url = '', descriptor = ''] = src.trim().split(' ');
-      let size = parseFloat(descriptor);
-      if (descriptor?.indexOf('x')>=0) {
-          size = Math.floor((node.width || node.clientWidth || 1) * size);
-      }
-      return {
-          url: url.trim(),
-          size,
-      };
-  })
-  .sort((a,b) => a.size < b.size ? 1 : (a.size > b.size ? -1 : 0));
-
-  // Use the largest size as the default.
-  let currentSrc = srcs[0].url;
-
-  node.ri = new ResizeObserver((entries) => {
-      requestAnimationFrame(() => {
-          for (const src of srcs) {
-              if (entries[0].contentRect.width * devicePixelRatio <= src.size){
-                  currentSrc = src.url;
+  // Create a new instance of MutationObserver
+  (new MutationObserver((mutationsList) => {
+      // Iterate over the mutations
+      for (let mutation of mutationsList) {
+          // Check if nodes were added
+          if (mutation.type === 'childList') {
+              // Iterate over the added nodes
+              for (let node of mutation.addedNodes) {
+                  if (node.nodeName === 'IMG' && !node.ri && node.srcset) {
+                      ri(node);
+                  }
+              }
+              for (let node of mutation.removedNodes) {
+                  if (node.nodeName === 'IMG' && node.ri) {
+                      node.ri.disconnect();
+                  }
               }
           }
-          node.src = currentSrc;
-      });
-  });
+      }
+  })).observe(document.documentElement, { childList: true, subtree: true });
 
-  node.ri.observe(node);
-}
-
-// Create a new instance of MutationObserver
-(new MutationObserver((mutationsList, observer) => {
-    // Iterate over the mutations
-    for (let mutation of mutationsList) {
-        // Check if nodes were added
-        if (mutation.type === 'childList') {
-            // Iterate over the added nodes
-            for (let node of mutation.addedNodes) {
-                // Check if the node is an <img> tag
-                if (node.nodeName === 'IMG' && !node.ri) {
-                    ri(node);
-                }
-            }
-            for (let node of mutation.removedNodes) {
-                if (node.nodeName === 'IMG' && node.ri) {
-                    node.ri.disconnect();
-                }
-            }
-        }
-    }
-})).observe(document.documentElement, { childList: true, subtree: true });
-document.querySelectorAll('img').forEach(ri);
+  // Initialise any images already added to the DOM.
+  document.querySelectorAll('img[srcset]').forEach(ri);
